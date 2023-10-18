14a80586e50f7cbce75e2d069f5f670a72b9ddd4
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-18 19:40:33 +0000

    DDL definition revised

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,8 +1,7 @@
-DROP TABLE IF EXISTS users        CASCADE;
-DROP TABLE IF EXISTS properties   CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
-DROP TABLE IF EXISTS addresses    CASCADE;
-DROP TABLE IF EXISTS reviews      CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -13,41 +12,39 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER NOT NULL DEFAULT 0,
-  parking_spaces INTEGER NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
-  thumbnail VARCHAR(255) NOT NULL,
-  cover VARCHAR(255) NOT NULL,
-  is_active BOOLEAN NOT NULL DEFAULT TRUE,
-  address_id INTEGER REFERENCES addresses(id) ON DELETE CASCADE
-  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
-);
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
-CREATE TABLE addresses (
-  id SERIAL PRIMARY KEY NOT NULL,
-  country VARCHAR(100) NOT NULL,
-  province VARCHAR(100) NOT NULL, 
-  city VARCHAR(100) NOT NULL,
-  street VARCHAR(100) NOT NULL,
-  post_code VARCHAR(6) NOT NULL
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
+
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  start_date DATE,
-  end_date DATE,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE 
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE reviews (
+CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  review TEXT,
-  stars SMALLINT NOT NULL DEFAULT 0
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
