e738b19ca8ae9ff3b768afe4c032526516383803
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-19 04:45:18 +0000

    converted get all properties to sql

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -14,7 +14,6 @@ const db = function($) {
   })
 }(process.env);
 
-console.log(db);
 
 /**
  * Get a single user from the database given their email.
@@ -73,11 +72,10 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return db
+    .query('SELECT * FROM properties LIMIT $1', [limit])
+    .then(res => res.rows)
+    .catch(err => console.log(err.message));
 };
 
 /**
